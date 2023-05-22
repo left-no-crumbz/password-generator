66f1f425e64f74a32159fc7e10b2842dc476db64
crumbz <kirsten.dwayne.dizon03@gmail.com>
2023-05-22 10:15:07 +0800

    Cleaner code. Final version maybe

--- Password Generator.py
+++ Password Generator.py
@@ -1,18 +1,25 @@
-import string, secrets, customtkinter, time
+import string, secrets, customtkinter
 import tkinter as tk
 
 
 class Password_Generator:
     def __init__(self):
+        super().__init__()
+        
+        # Enforces proper password length
+        self.INITIAL_LENGTH = 12
+        self.FINAL_LENGTH = 50
+        
         # Initializing the font for the GUI.
         self.title_font = ("Arial", 18)
         self.main_font = ("Arial", 16)
         self.sub_font = ("Arial", 12)
+        
         customtkinter.set_appearance_mode("system")
         customtkinter.set_default_color_theme("dark-blue")
         
         self.root = customtkinter.CTk()
-        self.root.geometry('720x1080')
+        self.root.geometry(f"{1100}x{580}")
         self.frame = customtkinter.CTkFrame(master=self.root)
         self.frame.pack(pady=20, padx=60, fill='both', expand=True)
         
@@ -20,31 +27,27 @@ class Password_Generator:
         self.title_label.pack(padx=12, pady=10)
         
         # Initializes the slider. The slider controls the password generation
-        self.password_length = customtkinter.CTkSlider(master=self.frame, from_=4, to=50, orientation='horizontal', hover=True, command=self.generate_password)
+        self.password_length = customtkinter.CTkSlider(master=self.frame, from_=self.INITIAL_LENGTH, to=self.FINAL_LENGTH, orientation='horizontal', hover=True, command=self.generate_password)
         self.password_length.pack(padx=12, pady=5)
         
         # Initializes the counter below the slider
         self.length = customtkinter.CTkEntry(master=self.frame, show='', font=self.sub_font, width=30, justify='center')
         self.length.pack(padx=2, pady=2)
         
+        # Initializes the checkboxes
         self.checkboxes = {
             "Letters": customtkinter.BooleanVar(value=True),
             "Numbers": customtkinter.BooleanVar(value=True),
             "Special": customtkinter.BooleanVar(value=True)
         }
-        # self.checkboxes.get("Letters") = customtkinter.BooleanVar(value=True)
-        # self.check_number = customtkinter.BooleanVar(value=True)
-        # self.check_special = customtkinter.BooleanVar(value=True)
-        
         self.check_lettercase = customtkinter.CTkCheckBox(self.frame, text="Letters", font=self.main_font, variable=self.checkboxes.get("Letters"))
         self.check_lettercase.pack(padx=2, pady=2)
-    
         self.check_numbercase = customtkinter.CTkCheckBox(self.frame, text="Numbers", font=self.main_font, variable=self.checkboxes.get("Numbers"))
         self.check_numbercase.pack(padx=2, pady=2)
-        
         self.check_specialcase = customtkinter.CTkCheckBox(self.frame, text="Special", font=self.main_font, variable=self.checkboxes.get("Special"))
         self.check_specialcase.pack(padx=2, pady=2)
-        
+    
+    
         self.show_password = customtkinter.CTkEntry(master=self.frame, show='', font=self.sub_font, width=365)
         self.show_password.pack(padx=10, pady=10)
         
@@ -52,7 +55,6 @@ class Password_Generator:
         self.copy.pack(padx=10, pady=10)
         
         self.root.mainloop()
-    
     def generate_password(self, password_length):
         self.length.delete(0, 'end')
         self.length.insert(0, int(password_length))
@@ -106,7 +108,9 @@ class Password_Generator:
         self.popup = customtkinter.CTkLabel(master=self.frame, text='Copied to Clipboard!', font=self.sub_font, fg_color='transparent')
         self.popup.pack(padx=5, pady=5)
         self.popup.after(2000, self.popup.destroy)
-Password_Generator()
+
+if __name__ == "__main__":
+    app = Password_Generator()
 
 
 
